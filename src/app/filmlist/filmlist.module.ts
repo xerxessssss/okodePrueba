c03891b240b4bdbfe83561fa446be830a37cfbae
Josep <skyrax11@gmail.com>
2022-03-04 21:01:40 +0100

    anterior y siguiente

--- src/app/filmlist/filmlist.module.ts
+++ src/app/filmlist/filmlist.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { FilmlistPage } from './filmlist.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    FilmlistPageRoutingModule
+    FilmlistPageRoutingModule,
+    Ng2SearchPipeModule
   ],
   declarations: [FilmlistPage]
 })
--- src/app/filmlist/filmlist.page.ts
+++ src/app/filmlist/filmlist.page.ts
@@ -10,8 +10,13 @@ import { HttpClient } from '@angular/common/http';
 export class FilmlistPage implements OnInit {
 
   popfilms = [];
+  pag: number;
+  searchTerm: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.pag = 1;
+    this.searchTerm = '';
+  }
 
   ngOnInit() {
     this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=ca05fd181a8a83164a05eb201faf6c9c&language=es&page=1')
@@ -21,4 +26,43 @@ export class FilmlistPage implements OnInit {
     });
   }
 
+  buscar(searchTerm) {
+    this.http.get<any>('https://api.themoviedb.org/3/search/movie?api_key=ca05fd181a8a83164a05eb201faf6c9c&query='
+      + searchTerm + '&language=es')
+    .subscribe(res => {
+      console.log('https://api.themoviedb.org/3/search/movie?api_key=ca05fd181a8a83164a05eb201faf6c9c&query='
+      + searchTerm + '&language=es');
+      this.popfilms = res.results;
+    });
+  }
+
+  siguiente() {
+    this.pag++;
+    this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=ca05fd181a8a83164a05eb201faf6c9c&language=es&page='
+      + this.pag)
+    .subscribe(res => {
+      console.log(res);
+      this.popfilms = res.results;
+    });
+  }
+
+  anterior() {
+    if(this.pag <= 1) {
+      this.pag = 1;
+      this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=ca05fd181a8a83164a05eb201faf6c9c&language=es&page='
+      + this.pag)
+    .subscribe(res => {
+      console.log(res);
+      this.popfilms = res.results;
+    });
+    } else {
+      this.pag--;
+      this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=ca05fd181a8a83164a05eb201faf6c9c&language=es&page='
+      + this.pag)
+    .subscribe(res => {
+      console.log(res);
+      this.popfilms = res.results;
+    });
+    }
+  }
 }
